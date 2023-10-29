55b763b04108addc28baccbb19775042bf786719
Shivansh Guleria <shivanshguleria@proton.me>
2023-10-29 22:17:21 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,39 +1,51 @@
-
-import Adafruit_BMP.BMP085 as BMP085
 import time
+from bmp280 import BMP280
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
 from signal import signal, SIGTERM, SIGHUP, pause
 from rpi_lcd import LCD
 import requests
+
+# Initialise the BMP280
+bus = SMBus(1)
+bmp280 = BMP280(i2c_dev=bus)
+
+
 lcd = LCD()
 
 def safe_exit(signum, frame):
     exit(1)
 
-url = "https://bmp-api-8jll.onrender.com/danger"
+url = "https://young-paper-8770.fly.dev/post"
 while True:
-    sensor = BMP085.BMP085()
-    t = 'Temp = {0:0.2f} *C'.format(sensor.read_temperature())
-    p = '{0:0.2f} Pa'.format(sensor.read_pressure())
-    a = '{0:0.2f} m'.format(sensor.read_altitude())
-    print(t+" "+p+" "+ a)
+    t = bmp280.get_temperature()
+    p = bmp280.get_pressure()
+    a = 0
+    print("[INFO] Data Generated")
+    print('{:05.2f}*C {:05.2f}hPa'.format(t, p))
     p_t, p_p, p_a = 0,0,0
     if p_t != t or p_p != p or p_a != a:
-            
+        print("[INFO] Conditions Met")
         payload = {
             't': t,
             'p': p,
             "a": a
         }
+        print("[INFO] Payload Generated")
         requests.post(url,json=payload)
+        print("[INFO] Data Posted to server")
         try:
             signal(SIGTERM, safe_exit)
             signal(SIGHUP, safe_exit)
-            lcd.text(f"Temprature {a}", 1)
-            lcd.text(f"Pressure {p}", 2)
-            pause()
+            lcd.clear()
+            lcd.text("{:05.2f}*C".format(t), 1)
+            lcd.text("{:05.2f}*hPa".format(p), 2)
+            time.sleep(2)
         except KeyboardInterrupt:
             pass
-        finally:
-            lcd.clear()
         p_t, p_a, p_p = t, a, p
-    time.sleep(3)
+    else:
+        print("Not")
+    time.sleep(1)
